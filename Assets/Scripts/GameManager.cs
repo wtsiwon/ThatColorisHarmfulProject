db0e8c43be8ab2023052c7b5c52c6336c1c23f3b
wtsiwon <siwon4727@gmail.com>
2023-04-17 18:05:12 +0900

    Score, SpdLevel선언

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    [Tooltip("속도가 증가하는 점수")]
+    public int speedIncreasePoint;
+
+    private int spdLevel;
+    public int SpdLevel
+    {
+        get => spdLevel;
+        set
+        {
+            spdLevel = value;
+            
+        }
+    }
+
+    private int score;
+    public int Score
+    {
+        get => score;
+        set
+        {
+            score = value;
+        }
+    }
+
     [Tooltip("현재 떨어지고 있는 오브젝트")]
     public Obj currentObj;
 
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : Singleton<UIManager>
+{
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+
+    [SerializeField]
+    private TextMeshProUGUI limitTime;
+
+}
